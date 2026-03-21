4cea37a94b5b8d40c42125f8ed93a47efe66a1ba
Dmitriy267 <dmitrio87@mail.ru>
2026-03-21 16:18:28 +0300

    style: добавил стили для компонентов к хуку useCallback

--- src/pages/UseCallbackList/UseCallbackList.jsx
+++ src/pages/UseCallbackList/UseCallbackList.jsx
@@ -1,13 +1,20 @@
 import { ParentComponent } from '../../components/ParentComponent/ParentComponent';
 import { InfinitUserScroll } from '../../components/InfinitUserScroll/InfinitUserScroll';
 import TodoListComponent from '../../components/TodoListComponent/TodoListComponent';
+import './UseCallbackList.css';
 export const UseCallbackList = () => {
     return (
         <>
-            <ParentComponent />
-            {/* <InfinitUserScroll /> */}
-            <h1>TodoListComponent</h1>
-            <TodoListComponent />
+            <section className="call__section">
+                <ParentComponent />
+            </section>
+            <section className="call__section">
+                <InfinitUserScroll />
+            </section>
+            <section className="call__section">
+                <h1>TodoListComponent</h1>
+                <TodoListComponent />
+            </section>
         </>
     );
 };
